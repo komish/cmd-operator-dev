287292112cd96340ad7c30f47945fe18889cf2c0
Jose R. Gonzalez <josegonzalez89@gmail.com>
2020-07-01 20:21:51 -0500

    Wed Jul  1 20:21:51 CDT 2020

--- pkg/controller/podrefresher/podrefresher_controller.go
+++ pkg/controller/podrefresher/podrefresher_controller.go
@@ -2,9 +2,12 @@ package podrefresher
 
 import (
 	"context"
+	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -16,7 +19,9 @@ import (
 )
 
 const (
-	certManagerIssuerKindAnnotation string = "cert-manager.io/issuer-kind"
+	certManagerIssuerKindAnnotation             string = "cert-manager.io/issuer-kind"
+	certManagerDeploymentAllowRestartAnnotation string = "certmanagerdeployment.redhat.io/allow-restart"
+	certManagerDeploymentRestartLabel           string = "certmanagerdeployment.redhat.io/time-restarted"
 )
 
 var log = logf.Log.WithName("controller_pod_refresher")
@@ -80,20 +85,81 @@ func (r *ReconcilePodRefresher) Reconcile(request reconcile.Request) (reconcile.
 	}
 
 	// If secret doesn't have cert-manager annotations, stop reconciliing it. This is the failsafe to prevent
-	// a bounce on a resource that is not a cert-manager secret.
+	// a bounce on a resource that is not a cert-manager-related secret.
 	ants := secret.GetAnnotations()
 	if _, ok := ants[certManagerIssuerKindAnnotation]; !ok {
-		reqLogger.Info("Secret is not a cert-manager issued certificate. Disregarding.", "Secret.Name", secret.GetName(), "Secret.Namespace", secret.GetNamespace())
+		reqLogger.V(2).Info("Secret is not a cert-manager issued certificate. Disregarding.", "Secret.Name", secret.GetName(), "Secret.Namespace", secret.GetNamespace())
 		return reconcile.Result{}, nil
 	}
 
 	reqLogger.Info("Secret is a cert-manager issued certificate.", "Secret.Name", secret.GetName(), "Secret.Namespace", secret.GetNamespace())
 
 	// If Secret has been updated, try to find deployments in the same namespace that needs to be bounced.
-	// IMPLEMENT
-	// If Deployment found, check it has opted-in to being bounced, otherwise stop
+	deployList := appsv1.DeploymentList{}
+	err = r.client.List(context.TODO(), &deployList, &client.ListOptions{Namespace: secret.GetNamespace()})
+	if err != nil {
+		reqLogger.Error(err, "Error listing deployments", "request.Namespace", secret.GetNamespace())
+		return reconcile.Result{}, err
+	}
 
-	// Update Deployment with annotations to force a bounce.
+	var updateFailed bool
+	for _, deploy := range deployList.Items {
+		updatedAt := time.Now().Format("2006-1-2.1504")
+		if hasAllowRestartAnnotation(deploy.ObjectMeta) && usesSecret(secret, deploy.Spec.Template.Spec) {
+			reqLogger.Info("Deployment opted-in to pod refreshes due to cert-manager secret update", "Deployment.Name", deploy.GetName(), "Secret.Name", secret.GetName())
+			updatedDeploy := deploy.DeepCopy() // TODO(): do we need to do this?
+			updatedDeploy.ObjectMeta.Labels[certManagerDeploymentRestartLabel] = updatedAt
+			updatedDeploy.Spec.Template.ObjectMeta.Labels[certManagerDeploymentRestartLabel] = updatedAt
+			err := r.client.Update(context.TODO(), updatedDeploy)
+			if err != nil {
+				reqLogger.Error(err, "Unable to restart deployment.", "Deployment.Name", deploy.GetName())
+				updateFailed = true
+			}
+		}
+	}
+
+	// Updating one of the deployments failed, requeue
+	// TODO(komish): This requeues if _any_ of the deployments fail, but this would cause a successful deployment to
+	// be restarted continuously. Need to requeue but with only the failed deployment.
+	if updateFailed {
+		reqLogger.Info("Requeuing due to inability to update a deployment associated with certmanager secret", "Secret.Name", secret.GetName(), "Secret.Namespace", secret.GetNamespace())
+		return reconcile.Result{}, err
+	}
 
 	return reconcile.Result{}, nil
+}
+
+// hasAllowRestartAnnotation returns true if the object meta has opted into restarts
+// via inclusion of a restart annotation, and false if the annotation is either there and
+// isn't set to true, or isn't set at all.
+// TODO() allow for arbitrary annotations to be checked in addition to our default for
+// backwards compatibility.
+func hasAllowRestartAnnotation(metadata metav1.ObjectMeta) bool {
+	annotations := metadata.GetAnnotations()
+	val, ok := annotations[certManagerDeploymentAllowRestartAnnotation]
+	// It's possible the value of the annotation is false, so check that the annotation
+	// is both present and true.
+	if val == "true" && ok {
+		return ok
+	}
+
+	return false
+}
+
+// usesSecret returns true if podspec contains a volume that is sourced from
+// a secret whose name matches the secret parameter, and false if it does not.
+func usesSecret(secret *corev1.Secret, podspec corev1.PodSpec) bool {
+	vols := podspec.Volumes
+
+	for _, vol := range vols {
+		// VolumeSource.Secret is a pointer, if it's uninitialized it should be nil
+		if secretRef := vol.VolumeSource.Secret; secretRef != nil {
+			// return true if we get a match
+			if secretRef.SecretName == secret.GetName() {
+				return true
+			}
+		}
+	}
+	// We didn't find a volume from a secret that matched our input secret.
+	return false
 }
