834384a565ce1f11e0de4ca097107f2d74866c66
Jose R. Gonzalez <josegonzalez89@gmail.com>
2020-09-22 10:57:11 -0500

    Tue Sep 22 10:57:11 CDT 2020

--- controllers/certmanagerdeployment_controller.go
+++ controllers/certmanagerdeployment_controller.go
@@ -54,7 +54,7 @@ type CertManagerDeploymentReconciler struct {
 
 // +kubebuilder:rbac:groups=operators.redhat.io,resources=certmanagerdeployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=operators.redhat.io,resources=certmanagerdeployments/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;create;update;patch;
+// +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;create;update;patch;watch;
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=namespaces;serviceaccounts;services,verbs=get;list;watch;create;update;patch;delete;
 // +kubebuilder:rbac:groups=core,resources=events,verbs=create;
