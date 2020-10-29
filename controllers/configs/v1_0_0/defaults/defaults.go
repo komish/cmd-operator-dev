9da78a09ca63649ea00770e816b476791a5cb98c
Jose R. Gonzalez <josegonzalez89@gmail.com>
2020-10-29 11:17:05 -0500

    Thu Oct 29 11:17:05 CDT 2020

--- controllers/configs/v1_0_0/defaults/defaults.go
+++ controllers/configs/v1_0_0/defaults/defaults.go
@@ -5,9 +5,9 @@ func ConfigForController() []byte {
 	return []byte(`apiVersion: certmanagerconfigs.operators.redhat.io/v1
 kind: CertManagerControllerConfig
 flags:
-	v: 2
-	cluster-resource-namespace: $(POD_NAMESPACE)
-	leader-election-namespace: $(POD_NAMESPACE)`)
+  v: 2
+  cluster-resource-namespace: $(POD_NAMESPACE)
+  leader-election-namespace: $(POD_NAMESPACE)`)
 }
 
 // ConfigForWebhook returns a default config for the webhook component as a byte slice of YAML.
@@ -15,21 +15,21 @@ func ConfigForWebhook() []byte {
 	return []byte(`apiVersion: certmanagerconfigs.operators.redhat.io/v1
 kind: CertManagerWebhookConfig
 flags:
-	v: 2
-	secure-port: 10250
-	dynamic-serving-ca-secret-namespace: $(POD_NAMESPACE)
-	dynamic-serving-ca-secret-name: cert-manager-webhook-ca
-	dynamic-serving-dns-names:
-	- cert-manager-webhook
-	- cert-manager-webhook.cert-manager
-	- cert-manager-webhook.cert-manager.svc`)
+  v: 2
+  secure-port: 10250
+  dynamic-serving-ca-secret-namespace: $(POD_NAMESPACE)
+  dynamic-serving-ca-secret-name: cert-manager-webhook-ca
+  dynamic-serving-dns-names:
+  - cert-manager-webhook
+  - cert-manager-webhook.cert-manager
+  - cert-manager-webhook.cert-manager.svc`)
 }
 
 // ConfigForCAInjector returns a default config for the webhook component as a byte slice of YAML.
 func ConfigForCAInjector() []byte {
 	return []byte(`apiVersion: certmanagerconfigs.operators.redhat.io/v1
-	kind: CertManagerCAInjectorConfig
-	flags:
-		v: 2
-		leader-election-namespace: $(POD_NAMESPACE)`)
+kind: CertManagerCAInjectorConfig
+flags:
+  v: 2
+  leader-election-namespace: $(POD_NAMESPACE)`)
 }
